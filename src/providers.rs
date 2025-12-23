e457433cce6a6a2b13dbd4c5c7552941dac11f29
Robert Campion <robert.b.campion@gmail.com>
2025-12-23 15:14:12 -0500

    Simplify error conversion using ? operator

--- src/providers.rs
+++ src/providers.rs
@@ -14,35 +14,27 @@ pub async fn update_provider(
     addr: IpAddr,
     domain: &DomainConfig,
 ) -> Result<Updates, Box<dyn std::error::Error>> {
-    match domain {
+    Ok(match domain {
         DomainConfig::Cloudflare(domain_config) => {
-            cloudflare::update_domains(http_client, domain_config, addr)
-                .await
-                .map_err(|e| e.into())
+            cloudflare::update_domains(http_client, domain_config, addr).await?
         }
         DomainConfig::GoDaddy(domain_config) => {
-            godaddy::update_domains(http_client, domain_config, addr)
-                .await
-                .map_err(|e| e.into())
+            godaddy::update_domains(http_client, domain_config, addr).await?
         }
         DomainConfig::Namecheap(domain_config) => {
-            namecheap::update_domains(http_client, domain_config, addr)
-                .await
-                .map_err(|e| e.into())
+            namecheap::update_domains(http_client, domain_config, addr).await?
         }
-        DomainConfig::He(domain_config) => he::update_domains(http_client, domain_config, addr)
-            .await
-            .map_err(|e| e.into()),
-        DomainConfig::NoIp(domain_config) => noip::update_domains(http_client, domain_config, addr)
-            .await
-            .map_err(|e| e.into()),
-        DomainConfig::Dynu(domain_config) => dynu::update_domains(http_client, domain_config, addr)
-            .await
-            .map_err(|e| e.into()),
+        DomainConfig::He(domain_config) => {
+            he::update_domains(http_client, domain_config, addr).await?
+        }
+        DomainConfig::NoIp(domain_config) => {
+            noip::update_domains(http_client, domain_config, addr).await?
+        }
+        DomainConfig::Dynu(domain_config) => {
+            dynu::update_domains(http_client, domain_config, addr).await?
+        }
         DomainConfig::Porkbun(domain_config) => {
-            porkbun::update_domains(http_client, domain_config, addr)
-                .await
-                .map_err(|e| e.into())
+            porkbun::update_domains(http_client, domain_config, addr).await?
         }
-    }
+    })
 }
